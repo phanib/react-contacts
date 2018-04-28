d1abfcd26ab455362e2a1b21ef7c624cc6710868
Phaneendra B <phani.brahma@gmail.com>
2018-04-28 11:28:45 -0500

    Added Contact List Component and Delete feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react'
+import ListContacts from './ListContacts';
 
 class App extends Component{
+    state = {
+        contacts: [
+          {
+            id: 'tyler',
+            name: 'Tyler McGinnis',
+            handle: '@tylermcginnis',
+            avatarURL: 'http://localhost:5001/tyler.jpg'
+          },
+          {
+            id: 'karen',
+            name: 'Karen Isgrigg',
+            handle: '@karen_isgrigg',
+            avatarURL: 'http://localhost:5001/karen.jpg'
+          },
+          {
+            id: 'richard',
+            name: 'Richard Kalehoff',
+            handle: '@richardkalehoff',
+            avatarURL: 'http://localhost:5001/richard.jpg'
+          },
+        ]
+      }
+    
+    removeContact = (contact) => {
+        this.setState((currentState) => (
+            {
+                contacts: currentState.contacts.filter((con)=> {
+                    return con.id !== contact.id
+                })
+            }
+        ))
+    }
+
     render() {
-        return (<div>
-            Hello World !
-        </div>)
+        return(
+            <div>
+                <ListContacts
+                    contacts={this.state.contacts}
+                    onDeleteContact={this.removeContact}
+                />
+            </div>
+        )
     }
 }
 
