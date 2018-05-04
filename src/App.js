1e9d5a63fd9efc687a4feddb696127f87c12fe98
Phaneendra B <phani.brahma@gmail.com>
2018-05-03 20:30:42 -0500

    Create Contact Form Completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts';
 import * as ContactsAPI from './utils/ContactsAPI'
+import { Route } from 'react-router-dom';
+import CreateContact from './CreateContact'
 
 class App extends Component{
     state = {
@@ -30,10 +32,13 @@ class App extends Component{
     render() {
         return(
             <div>
-                <ListContacts
+                <Route exact path="/" render={() => (<ListContacts
                     contacts={this.state.contacts}
                     onDeleteContact={this.removeContact}
+                    />)} 
                 />
+                <Route path="/create" component={CreateContact} />
+ 
             </div>
         )
     }
